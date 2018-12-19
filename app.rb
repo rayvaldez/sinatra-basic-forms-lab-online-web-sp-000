4ddce5e7a37f9dd379c2012dfd7c7d58661f9270
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-12-19 17:02:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,8 +11,8 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    @new_puppy = Puppy.new(params)
-    
+    @puppy = Puppy.new(params['name'], params['breed'], params['age'])
+
     erb :display_puppy
   end
 end
